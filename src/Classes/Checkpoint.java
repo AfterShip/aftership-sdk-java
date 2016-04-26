39e8d44a80fff0f22698c91d7d7156a5c6bfb191
lsozke <glen@aftership.net>
2016-04-26 17:53:40 +0800

    (1) Add slug property in Checkpoint, (2) beatify Tracking.toString() by removing single quote

--- src/Classes/Checkpoint.java
+++ src/Classes/Checkpoint.java
@@ -46,6 +46,9 @@ public class Checkpoint {
     /** Location info (if any) */
     private String location;
 
+    /** Slug */
+    private String slug;
+
     public Checkpoint(JSONObject checkPointJSON) throws JSONException{
 
         this.createdAt = checkPointJSON.isNull("created_at")?null:checkPointJSON.getString("created_at");
@@ -59,6 +62,7 @@ public class Checkpoint {
         this.tag = checkPointJSON.isNull("tag")?null:checkPointJSON.getString("tag");
         this.zip = checkPointJSON.isNull("zip")?null:checkPointJSON.getString("zip");
         this.location = checkPointJSON.isNull("location")?null:checkPointJSON.getString("location");
+        this.slug = checkPointJSON.isNull("slug")?null:checkPointJSON.getString("slug");
 
     }
 
@@ -66,6 +70,8 @@ public class Checkpoint {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("\n\tCheckpoint{");
+        sb.append((slug==null)?"":"\n\t\tslug="+slug);
+        sb.append((createdAt==null)?"":"\n\t\tcreatedAt="+createdAt);
         sb.append((checkpointTime==null)?"":"\n\t\tcheckpointTime="+checkpointTime);
         sb.append((city==null)?"":"\n\t\tcity="+city);
         sb.append((countryISO3==null)?"":"\n\t\tcountryISO3="+countryISO3);
@@ -117,6 +123,10 @@ public class Checkpoint {
 
     public String getLocation() {
         return location;
+    }
+
+    public String getSlug() {
+        return slug;
     }
 }
 
